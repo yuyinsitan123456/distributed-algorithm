e850a29244ab686769667701f089885fc139ee8d
Hogan <hsbzzhz@126.com>
2017-05-14 15:12:52 +1000

    initial

--- src/core/Accepter.java
+++ src/core/Accepter.java
@@ -1,5 +1,19 @@
 package core;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+
 public class Accepter {
 	static class Instance{
 		//current ballot number
@@ -22,5 +36,11 @@ public class Accepter {
 	}
 	//
 // accept's state, contain each instances 
-	private Map<Integer, Instance> instanceState=
+	private Map<Integer, Instance> instanceState = new HashMap<>();
+	//accepted value
+	private Map<Integer, Object> acceptedValue = new HashMap<>();
+	//accepter's id
+	private transient int id;
+	//proposals
+	
 }

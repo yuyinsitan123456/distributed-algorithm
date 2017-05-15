705351cc380c9627271678b459ddc7595148bf47
yuyinsitan123456 <yuyinsitan@gmail.com>
2017-05-15 20:08:42 +1000

    paxosServerAdd

---
+++ src/paxosBank/test.java
@@ -0,0 +1,11 @@
+package paxosBank;
+
+import java.io.IOException;
+
+import paxosServer.PaxosServer;
+
+public class test {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		new PaxosServer("config.json");
+	}
+}
